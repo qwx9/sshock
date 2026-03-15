eae01ba22b918a43769b76116bc2fefda40f6edc
qwx <qwx@sciops.net>
2026-03-15 06:34:05 +0000

    __FUNCTION__: nope try again

--- src/Libraries/H/platform.h
+++ src/Libraries/H/platform.h
@@ -5,7 +5,7 @@
 #ifndef	__FUNCTION__
 #define	__S1(x)	#x
 #define	__S2(x)	__S1(x)
-#define __FUNCTION__	__S2(__func__) ": " __FILE__ ":" __S2(__LINE__)
+#define __FUNCTION__	__FILE__ ":" __S2(__LINE__)
 #endif
 
 #endif
